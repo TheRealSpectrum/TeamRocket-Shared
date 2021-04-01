30d784c10240ed2578fa6a0bfcd775212b3e680f
Matthew <mwildhagen@hotmail.com>
2021-04-01 16:09:25 +0200

    changes

--- raadgame.php
+++ raadgame.php
@@ -1,19 +1,34 @@
 <?php
 
+$x   = rand(1, 100);
+$num = '';
 
+if (isset($_POST['submit'])) {
+
+
+        if ($num < $x) 
+        {
+            echo " Your number is lower! <br />";
+        } elseif ($num > $x) 
+            {
+            echo " Your number is higher! <br />";
+        } elseif ($num == $x) 
+            {
+            echo " Congratulations! You guessed the hidden number. <br />";
+        } else 
+            {
+            echo " You never set a numberrr! <br />";
+        }
+
+
+
+
+}
 ?>
-
-<html>
-
-    <body>
-
-        <form action="/raadgame.php">
-        
-
-
-
-        </form>
-
-    </body>
-
-</html>
+<p>
+<form action="" method="post">
+<input type="text" name="num">
+<button type="submit" name="submit">Guess</button>
+<button type="reset" name="Reset">Reset</button>
+</form>
+</p>
