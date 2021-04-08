f954cb63646702bd1bda7e263eb2283a10d5a84c
Matthew <mwildhagen@hotmail.com>
2021-04-08 16:25:08 +0200

    update

--- raadgame.php
+++ raadgame.php
@@ -16,6 +16,9 @@ else
 {
     $_SESSION['counter']++;
 }
+if(!isset($_SESSION['guess'])){
+    $_SESSION['guess'] = array();
+}
 
 $rand = $_SESSION['the_number'];
 $counter = $_SESSION['counter'];
@@ -24,10 +27,9 @@ $guess = isset($_POST['guess']) ? (int) $_POST['guess'] : false;
 
 if($guess == $rand)
 {
-    // If the user guessed the number, unset counter & rand number
-    // This way, on the next refresh, the game will restart
     unset($_SESSION['the_number']);
     unset($_SESSION['counter']);
+    unset($_SESSION['guess']);
 }
 
 
@@ -52,7 +54,7 @@ if ($guess != false)
     
     print "<hr />";
     print "The number you input is $guess <br />";
-    print "The numbers guessed so far: $n <br />";
+    print "The numbers guessed so far:" . $_SESSION['guess'] . "<br />";
 
     if ($guess == $rand)
     {
@@ -89,17 +91,6 @@ if ($guess != false)
 <?php else: ?>
 <a href="raadgame.php">Press Here to Restart</a>
 <?php endif; ?>
-
-<!--
-
-The Random Number: <?php echo $rand; ?>
-
-The Counter: <?php echo $counter; ?>
-
-The Guess: <?php echo htmlspecialchars($guess); ?>
-
-
--->
 
 </body>
 </html>
