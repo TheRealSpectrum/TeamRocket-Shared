77e3e1a1fc7c53a5087c3432a3bb555d8b39cadf
Matthew <mwildhagen@hotmail.com>
2021-04-15 15:29:11 +0200

    update

--- gastenboek/index.php
+++ gastenboek/index.php
@@ -1,11 +1,9 @@
-<h1>My Guestbook</h1>
-<a href="index.php" class="gb-link"><p>View Guestbook</p></a>
-<p class="divider">|</p>
-<a href="form.php" class="msg-link"><p>Leave a Message</p></a>
-<p class="rsps">
-  <?php
-  $guests = fopen("guests.txt", "r") or die("Unable to open file!");
-  echo fread($guests,filesize("guests.txt"));
-  fclose($guests);
-  ?>
-</p>
+<h1>Guestbook</h1>
+<a href="index.php" class="gb-link">View Guestbook</a>
+<a href="form.php" class="msg-link">Leave a Message</a>
+
+<?php
+$guests = fopen("guests.txt", "r") or die("Unable to open file!");
+echo fread($guests,filesize("guests.txt"));
+fclose($guests);
+?>
