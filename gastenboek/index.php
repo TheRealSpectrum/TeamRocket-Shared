44e34d9885a66887aebf5c36be9d4fa22a7f0a65
Matthew <mwildhagen@hotmail.com>
2021-04-16 12:30:12 +0200

    update

--- gastenboek/index.php
+++ gastenboek/index.php
@@ -5,6 +5,7 @@
 <title>Guestbook</title>
 </head>
 <body>
+
     <h1>Guestbook</h1>
     <button>
       <a href="index.php" class="gb-link">View Guestbook</a>
@@ -13,11 +14,31 @@
       <a href="form.php" class="msg-link">Leave a Message</a>
     </button>
     <div class=messagecontainer>
-      <?php
-      $guests = fopen("guests.txt", "r") or die("Unable to open file!");
-      echo file_get_contents('guests.txt');
-      fclose($guests);
-      ?>
     </div>
+
+    <?php
+    
+    $guests = fopen("guests.txt", "r") or die("Unable to open file!");
+    echo file_get_contents('guests.txt');
+    fclose($guests);
+
+    function button1() {
+    // Delete all comments          WORK IN PROGRESS
+    $a = 'guests.txt';
+    $b = file_get_contents('guests.txt');
+    $c = preg_replace('guests.txt', '', $b);
+    file_put_contents($a, $c);
+    }
+
+    if(array_key_exists('delete', $_POST)) {
+      button1();
+    }
+    
+    ?>
+    
+    <form method="post">
+      <input type="submit" name="delete" class="button" value="delete all" />
+    </form>
+
 </body>
 </html>
