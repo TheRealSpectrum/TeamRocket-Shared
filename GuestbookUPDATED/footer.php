fc4c1030f3996e250ed76c9db539e60cb9947775
Matthew <mwildhagen@hotmail.com>
2021-05-25 16:22:02 +0200

    update

--- GuestbookUPDATED/footer.php
+++ GuestbookUPDATED/footer.php
@@ -1,8 +1,9 @@
     </div>
-</body>
-<footer>
+ <footer>
     <p>Copyright© 2021 Team Rocket. We own this!</p>
 </footer>
+<script src="scripts.js"></script>
+</body>
 </html>
 <?php
 // Sluit de html af met een footer.
