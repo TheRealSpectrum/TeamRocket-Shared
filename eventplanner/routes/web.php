c61e5f6dc92073f3d4acf53d21bd008fc6c08d2e
Matt123455 <mwildhagen@hotmail.com>
2021-06-08 02:01:38 +0200

    update

---
+++ eventplanner/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use \App\Models\Event;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/events', function () {
+    return view('events')->with(['events'=> Event::all()]);
+})->middleware(['auth', 'verified'])->name('events');
+
+require __DIR__.'/auth.php';
