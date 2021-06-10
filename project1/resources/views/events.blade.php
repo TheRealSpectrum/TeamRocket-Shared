11f71cde79fbc4d065dd6825ab21d40a7cb97f17
Matthew <mwildhagen@hotmail.com>
2021-06-10 09:35:31 +0200

    update

---
+++ project1/resources/views/events.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Events') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                @foreach($events as $event)
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <h2 class="text-xl font-medium">{{ $event->title }}</h2>
+                        <p class="text-gray-600 leading-relaxed">{{ $event->body }}</p>
+                        <p class="text-gray-600 leading-relaxed">{{ $event->created_at }}</p>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</x-app-layout>
